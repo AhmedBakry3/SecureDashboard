1da2ea0af5d2796a7f54de487a7b3e05e6f94514
Ahmed Bakry <ahmed.elbakry.ab@gmail.com>
2025-03-23 03:00:51 +0200

    Part 03 Department Controller Index

---
+++ Demo.Presentation/Controllers/DepartmentsController.cs
@@ -0,0 +1,16 @@
+﻿using Demo.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Presentation.Controllers
+{
+    public class DepartmentsController(IDepartmentService _departmentService) : Controller
+    {
+        //Get BaseURL/Departments/Index
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var Departments = _departmentService.GetAllDepartments();    
+            return View(Departments);
+        }
+    }
+}
