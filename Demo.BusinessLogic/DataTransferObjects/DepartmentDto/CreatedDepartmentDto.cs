0c15f2b181685c0cb9dc018ee1bdd18fedaba867
Ahmed Bakry <ahmed.elbakry.ab@gmail.com>
2025-04-04 22:17:15 +0200

    Part 10 Client Side Validation

--- Demo.BusinessLogic/DataTransferObjects/DepartmentDto/CreatedDepartmentDto.cs
+++ Demo.BusinessLogic/DataTransferObjects/DepartmentDto/CreatedDepartmentDto.cs
@@ -5,11 +5,8 @@ namespace Demo.BusinessLogic.DataTransferObjects.DepartmentDto
 {
     public class CreatedDepartmentDto
     {
-        [Required(ErrorMessage = "Name is Required !!!!!!")]
         public string Name { get; set; } = null!;
-        public string Description { get; set; }
-        [Required]
-        [Range(100, int.MaxValue)]
+        public string? Description { get; set; }
         public string Code { get; set; } = null!;
         public DateOnly DateOfCreation { get; set; }
     }
