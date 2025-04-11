0cba0dadc179a34c968453a82a7bcd5e43982697
Ahmed Bakry <ahmed.elbakry.ab@gmail.com>
2025-04-11 07:22:22 +0200

    Part 06 Install Package and Dbcontext

--- Demo.DataAccess/Data/Contexts/ApplicationDbContext.cs
+++ Demo.DataAccess/Data/Contexts/ApplicationDbContext.cs
@@ -1,24 +1,27 @@
 ﻿
 using Demo.DataAccess.Data.Configurations;
 using Demo.DataAccess.Models.DepartmentModel;
+using Demo.DataAccess.Models.IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Demo.DataAccess.Data.Contexts
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
         public DbSet<Department> Departments { get; set; }  
         public DbSet<Employee> Employees { get; set; }
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer("ConnectionString");
-        //}
+        
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             //modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
 
         }
     }
