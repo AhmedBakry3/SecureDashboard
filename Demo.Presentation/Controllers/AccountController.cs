d935960c90cf02dcc083506fa99dd45e41531965
Ahmed Bakry <ahmed.elbakry.ab@gmail.com>
2025-04-11 08:13:53 +0200

    Part 07 AuthLayout

---
+++ Demo.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        //Register
+        public IActionResult Register() => View();
+        //Login
+        //Sign Out
+    }
+}
